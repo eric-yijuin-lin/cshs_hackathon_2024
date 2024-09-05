9d7428f2e916675823f9b7dad0432e41c5ef4aa8
Eric Lin <eric-yijuin-lin@gmail.com>
2024-09-05 17:58:26 +0800

    update esp32 servo test

--- test/esp32_servo/esp32_servo.ino
+++ test/esp32_servo/esp32_servo.ino
@@ -1,12 +1,15 @@
 /*********
- * 手邊有的 & 適合做垃圾桶的伺服馬達是 360度的 MG996R
- * 無法控制角度，所以目前用速度與 delay 時間硬幹
- * 目前測試， servo.write(130) 配合 delay (320) 差不多剛好 1 圈
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp32-servo-motor-web-server-arduino-ide/
+  Based on the ESP32Servo Sweep Example
 *********/
 
 #include <ESP32Servo.h>
 
 static const int servoPin = 13;
+static const int servoRotateSpeed = 130;
+static const int servoStopSpeed = 100;
+static const double time_of_degree = (double)3180 / 360;
 
 Servo servo1;
 int test = 100;
@@ -14,23 +17,52 @@ int test = 100;
 void setup() {
   Serial.begin(115200);
   servo1.attach(servoPin, 1000, 2000);
-  for (int i=0; i<10; i++) {
-    Serial.println("hello, ");
-    Serial.println(i);
-  }
 }
 
 void loop() {
-  for (int i=0; i<10; i++) {
-    if (i % 2 == 0) {
-      servo1.write(130);
-      Serial.println("going~");
-      delay(3200);
+  // for (int i=0; i<10; i++) {
+  //   if (i % 2 == 0) {
+  //     Serial.println("going~");
+  //     servo1.write(130);
+  //     delay(3126);
+  //   }
+  //   else {
+  //     servo1.write(100);
+  //     Serial.println("take a break");
+  //     delay(1000);
+  //   }
+  // }
+
+
+  if (Serial.available()) {
+    char c = Serial.read();
+    int targetDegree = 0;
+    switch(c) {
+      case 'w': targetDegree = 0; break;
+      case 'a': targetDegree = 90; break;
+      case 's': targetDegree = 180; break;
+      case 'd': targetDegree = 270; break;
+      default: targetDegree = 0; break;
     }
-    else {
-      servo1.write(100);
-      Serial.println("take a break");
-      delay(3000);
-    }
+
+    double goTime = targetDegree * time_of_degree;
+    double backTime = (360 - targetDegree) * time_of_degree;
+    Serial.print("targetDegree: ");
+    Serial.print(targetDegree);
+    Serial.print("goTime: ");
+    Serial.print(goTime);
+    Serial.print("backTime: ");
+    Serial.print(backTime);
+
+    servo1.write(servoRotateSpeed);
+    delay(goTime);
+
+    servo1.write(servoStopSpeed);
+    delay(2000);
+
+    servo1.write(servoRotateSpeed);
+    delay(backTime);
+    servo1.write(servoStopSpeed);
+    delay(20);
   }
 }
